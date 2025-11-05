ae1a8a8169acfb397e882fbafcd1a52b2375d6ed
a1963055 <a1963055@adelade.edu.au>
2025-11-05 14:16:43 +0000

    created a simplified gps tracker

--- GpsGUI.java
+++ GpsGUI.java
@@ -2,10 +2,27 @@ import nz.sodium.*;
 import javax.swing.*;
 import java.awt.*;
 
+class SimplifiedGps {
+    String name;
+    double latitude;
+    double longitude;
+    
+    SimplifiedGps(String name, double latitude, double longitude) {
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+}
+
 public class GpsGUI {
     public static void main(String[] args) {
         GpsService serv = new GpsService();
         Stream<GpsEvent>[] streams = serv.getEventStreams();
+        
+        Stream<SimplifiedGps>[] simplified = new Stream[streams.length];
+        for(int i = 0; i < streams.length; i++) {
+            simplified[i] = streams[i].map(ev -> new SimplifiedGps(ev.name, ev.latitude, ev.longitude));
+        }
         
         JFrame frame = new JFrame("GPS Tracker");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
